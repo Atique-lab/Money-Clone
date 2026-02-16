12acdd92578f854b794ea0f52b0682ff76e24a6e
Atique-lab <shaikhatique693@gmail.com>
2026-02-16 23:49:43 +0530

    Correct Production Build

--- dist/src/components/Section.jsx
+++
@@ -1,9 +0,0 @@
-function Section({ children, className = "" }) {
-    return (
-        <section className={`relative overflow-hidden ${className}`}>
-            {children}
-        </section>
-    );
-}
-
-export default Section;
